bb44c5781b1d512e9982ec8cec3010a7ab5db95d
Andrew Davisson <akdavisson4@gmail.com>
2019-11-21 16:18:22 -0700

    Removed build from .gitignore

---
+++ build/precache-manifest.10a8801f3e3e878e822fd08f9731acb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e982da99ee01eadf9329bdc9b500af21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53e71b4a7c560ccb1c62",
+    "url": "/static/css/2.5158945e.chunk.css"
+  },
+  {
+    "revision": "b8bb5c7d92af724897ce",
+    "url": "/static/css/main.f18a4d3e.chunk.css"
+  },
+  {
+    "revision": "53e71b4a7c560ccb1c62",
+    "url": "/static/js/2.bae0a76d.chunk.js"
+  },
+  {
+    "revision": "b8bb5c7d92af724897ce",
+    "url": "/static/js/main.defbcb97.chunk.js"
+  },
+  {
+    "revision": "ae970b72d1121cf53cb6",
+    "url": "/static/js/runtime-main.3664b334.js"
+  },
+  {
+    "revision": "1d18486260bebae1b1826c4df6ecea17",
+    "url": "/static/media/andrew-resume.1d184862.pdf"
+  }
+]);
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="Home">
+    <div className="home">
       <h1>Welcome!</h1>
+      <img src="https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif"/>
+      <p>Welcome! I am glad you are here. Please browse this site to learn more about me. I also appreciate your patience while this site is under maintenance.</p>
     </div>
   );
 }
