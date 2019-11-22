b30f6e765ac9903c0467ada4e0473df11d0e4d03
Andrew Davisson <akdavisson4@gmail.com>
2019-11-21 18:27:21 -0700

    Added social to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 const NavBar = () => {
   return (
-    <div className="navbar navbar-expand-md">
-      <NavLink to="/" className="nav-link">Home</NavLink>
-      <NavLink to="/resume" className="nav-link">Resume</NavLink>
-      <NavLink to="/about" className="nav-link">About</NavLink>
-    </div>
+    <Navbar>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/resume">Resume</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <NavDropdown title="Social" id="basic-nav-dropdown">
+            <NavDropdown.Item href="https://www.linkedin.com/in/andrew-davisson/">LinkedIn</NavDropdown.Item>
+            <NavDropdown.Item href="https://github.com/adavisson">GitHub</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
  
