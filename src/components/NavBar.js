1d07e7e7e43098942ba86ef5d227594b5dae0fe6
Andrew Davisson <akdavisson4@gmail.com>
2019-11-21 18:33:59 -0700

    updated navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link  href="/">Home</Nav.Link>
           <Nav.Link href="/resume">Resume</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <NavDropdown title="Social" id="basic-nav-dropdown">
