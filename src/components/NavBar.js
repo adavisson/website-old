60fa0997b9fd21a6ab5bee30c7e9964725c1f6ae
Andrew Davisson <akdavisson4@gmail.com>
2019-11-21 19:26:16 -0700

    Edited Navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,12 +8,12 @@ const NavBar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link  href="/">Home</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/resume">Resume</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <NavDropdown title="Social" id="basic-nav-dropdown">
-            <NavDropdown.Item href="https://www.linkedin.com/in/andrew-davisson/">LinkedIn</NavDropdown.Item>
-            <NavDropdown.Item href="https://github.com/adavisson">GitHub</NavDropdown.Item>
+            <NavDropdown.Item target="_blank" href="https://www.linkedin.com/in/andrew-davisson/">LinkedIn</NavDropdown.Item>
+            <NavDropdown.Item target="_blank" href="https://github.com/adavisson">GitHub</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
