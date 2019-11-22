c85506beb542a4ae5313afd579248b40be525e01
Andrew Davisson <akdavisson4@gmail.com>
2019-11-22 09:14:06 -0700

    Fixed spelling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="home">
       <h1>Welcome!</h1>
-      <Image src="https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif" fluid/>
+      <Image src="https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif" rounded fluid/>
       <p>Welcome, I am glad you are here! Please browse this site to learn more about me. I also appreciate your patience while this site is under maintenance.</p>
     </div>
   );
