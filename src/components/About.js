afb2c9ce933443afe8675c1b14093456fe14f4cd
Andrew Davisson <akdavisson4@gmail.com>
2019-11-21 22:38:31 -0700

    Added profile picture

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import pic from '../files/profile_pic_crop.JPG';
 
 const About = () => {
   return (
     <div className="about">
       <h2>About Me</h2>
+      <Image src={pic} fluid />
     </div>
   );
 }
