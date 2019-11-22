cfa04e2dceb842b229d780817b56b10d6a821953
Andrew Davisson <akdavisson4@gmail.com>
2019-11-21 23:29:32 -0700

    More NavBar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 const NavBar = () => {
   return (
     <Navbar sticky="top" expand="lg">
+      <Navbar.Brand href="/">Home</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/resume">Resume</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <NavDropdown title="Social" id="basic-nav-dropdown">
