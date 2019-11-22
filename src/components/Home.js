f084c5837f7fcdca9c25d71c5934d6fbe39aebdf
Andrew Davisson <akdavisson4@gmail.com>
2019-11-21 21:59:22 -0700

    Updated styling to work better on mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image'
 
 const Home = () => {
   return (
     <div className="home">
       <h1>Welcome!</h1>
-      <img src="https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif"/>
+      <Image src="https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif" fluid/>
       <p>Welcome! I am glad you are here. Please browse this site to learn more about me. I also appreciate your patience while this site is under maintenance.</p>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
-const NavBar = () => {
-  return (
-    <Navbar>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/resume">Resume</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
-          <NavDropdown title="Social" id="basic-nav-dropdown">
-            <NavDropdown.Item target="_blank" href="https://www.linkedin.com/in/andrew-davisson/">LinkedIn</NavDropdown.Item>
-            <NavDropdown.Item target="_blank" href="https://github.com/adavisson">GitHub</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+class NavBar extends Component {
+  state = {
+    isMobile: false
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', () => {
+        this.setState({
+            isMobile: window.innerWidth < 800
+        });
+    }, false);
+  }
+
+  render() { 
+    const mobile = this.state.isMobile ? '' : 'lg';
+    return (
+      <Navbar expand={mobile}>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/resume">Resume</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <NavDropdown title="Social" id="basic-nav-dropdown">
+              <NavDropdown.Item target="_blank" href="https://www.linkedin.com/in/andrew-davisson/">LinkedIn</NavDropdown.Item>
+              <NavDropdown.Item target="_blank" href="https://github.com/adavisson">GitHub</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
  
 export default NavBar;
