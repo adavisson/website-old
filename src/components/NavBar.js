c7173cf4d3a8b4b5227e25c8234686886db53d44
Andrew Davisson <akdavisson4@gmail.com>
2019-11-21 23:25:52 -0700

    Simplified NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
-class NavBar extends Component {
-  state = {
-    isMobile: false
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', () => {
-        this.setState({
-            isMobile: window.innerWidth < 800
-        });
-    }, false);
-  }
-
-  render() { 
-    const mobile = this.state.isMobile ? '' : 'lg';
-    return (
-      <Navbar sticky="top" expand={mobile}>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/resume">Resume</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <NavDropdown title="Social" id="basic-nav-dropdown">
-              <NavDropdown.Item target="_blank" href="https://www.linkedin.com/in/andrew-davisson/">LinkedIn</NavDropdown.Item>
-              <NavDropdown.Item target="_blank" href="https://github.com/adavisson">GitHub</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+const NavBar = () => {
+  return (
+    <Navbar sticky="top" expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/resume">Resume</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <NavDropdown title="Social" id="basic-nav-dropdown">
+            <NavDropdown.Item target="_blank" href="https://www.linkedin.com/in/andrew-davisson/">LinkedIn</NavDropdown.Item>
+            <NavDropdown.Item target="_blank" href="https://github.com/adavisson">GitHub</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
  
 export default NavBar;
