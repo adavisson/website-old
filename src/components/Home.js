36fadcfd574f46ec7e4057adbbfe73780015d344
Andrew Davisson <akdavisson4@gmail.com>
2019-11-22 09:03:00 -0700

    Added text to about section

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="home">
       <h1>Welcome!</h1>
       <Image src="https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif" fluid/>
-      <p>Welcome! I am glad you are here. Please browse this site to learn more about me. I also appreciate your patience while this site is under maintenance.</p>
+      <p>Welcome, I am glad you are here! Please browse this site to learn more about me. I also appreciate your patience while this site is under maintenance.</p>
     </div>
   );
 }
