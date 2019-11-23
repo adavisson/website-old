8a319f2f6903570336b7bfbc1c7880fb6d3276fb
Andrew Davisson <akdavisson4@gmail.com>
2019-11-22 23:10:31 -0700

    fixed resume view for now

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,6 +5,10 @@ import resume from "../files/andrew-resume.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume = () => {
+  let w = null;
+  if (window.innerWidth < 800){
+    w = window.innerWidth * .9;
+  }
   return (
     <div className="resume">
       <h1>Resume</h1>
@@ -13,7 +17,7 @@ const Resume = () => {
       </div>
       <div className="document">
         <Document file={resume}>
-          <Page pageNumber={1}/>
+          <Page pageNumber={1} width={w}/>
         </Document>
       </div>
     </div>
