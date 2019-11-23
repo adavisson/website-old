2a5a629d7c4319e9886743e01ecf248b2813a5f1
Andrew Davisson <akdavisson4@gmail.com>
2019-11-22 23:25:23 -0700

    Resume resizes properly now

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,27 +1,51 @@
-import React from 'react';
+//import React from 'react';
+import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
 import resume from "../files/andrew-resume.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const Resume = () => {
-  let w = null;
-  if (window.innerWidth < 800){
-    w = window.innerWidth * .9;
+class Resume extends Component {
+  state = {
+    w: window.innerWidth
   }
-  return (
-    <div className="resume">
-      <h1>Resume</h1>
-      <div className="link">
-        <a target="_blank" href={resume}>Download a copy</a>
-      </div>
-      <div className="document">
-        <Document file={resume}>
-          <Page pageNumber={1} width={w}/>
-        </Document>
-      </div>
-    </div>
-  );
+
+  componentDidMount(){
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => {
+    if(window.innerWidth < 700){
+      this.setState({
+        w: window.innerWidth * .9
+      })
+    } else {
+      this.setState({
+        w: null
+      })
+    }
+  }
+
+  render() { 
+    return (
+      <div className="resume">
+       <h1>Resume</h1>
+       <div className="link">
+         <a target="_blank" href={resume}>Download a copy</a>
+       </div>
+       <div className="document">
+         <Document file={resume}>
+           <Page pageNumber={1} width={this.state.w}/>
+         </Document>
+       </div>
+     </div>
+    );
+  }
 }
  
 export default Resume;
