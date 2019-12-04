db4d037ffef23d03ef7c596e697808832b0fb0fd
Andrew Davisson <akdavisson4@gmail.com>
2019-12-04 08:45:04 -0700

    Added flexbox to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="home">
       <h1>Welcome!</h1>
-      <Image src="https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif" rounded fluid/>
+      <Image className="gif" src="https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif" rounded fluid/>
       <p>Welcome, I am glad you are here! Please browse this site to learn more about me, and feel free to reach out and contact me if you have any questions.</p>
     </div>
   );
