bbf02c56d51484b6f2ed018c0ad9a8f5a6c41d52
Andrew Davisson <akdavisson4@gmail.com>
2020-01-07 09:48:59 -0700

    Moved link to blog from being nested

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,12 +9,12 @@ const NavBar = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/resume">Resume</Nav.Link>
+          <Nav.Link href="https://adavisson.github.io">Blog</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <NavDropdown title="Social" id="basic-nav-dropdown-social">
             <NavDropdown.Item target="_blank" href="https://www.linkedin.com/in/andrew-davisson/">LinkedIn</NavDropdown.Item>
             <NavDropdown.Item target="_blank" href="https://github.com/adavisson">GitHub</NavDropdown.Item>
             <NavDropdown.Item target="_blank" href="https://www.builtincolorado.com/member/akdavisson4/176086">Built In Colorado</NavDropdown.Item>
-            <NavDropdown.Item target="_blank" href="https://adavisson.github.io/">Blog</NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title="Projects" id="basic-nav-dropdown-projects">
             <NavDropdown.Item href="https://crazy-tales.andydavisson.com">Crazy Tales</NavDropdown.Item>
