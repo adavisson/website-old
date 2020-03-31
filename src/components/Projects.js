3926dd08166fcaead36817051574fd0359464fb7
Andrew Davisson <akdavisson4@gmail.com>
2020-03-31 07:31:30 -0600

    styling for cards to match colors

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import ProjectCard from './ProjectCard';
 
 const Projects = () => {
   return (
-    <h1>Projects</h1>
+    <>
+      <h1>Projects</h1>
+      <div className="card-container">
+        <ProjectCard
+          title="CFB Machine"
+          description="A football website"
+        />
+      </div>
+    </>
   );
 }
  
