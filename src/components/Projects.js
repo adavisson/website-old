b9538eabbe44c9157927fdf879645feb150b4d7a
Andrew Davisson <akdavisson4@gmail.com>
2020-03-31 07:38:22 -0600

    refactored to keep prjects in object

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,14 +2,39 @@ import React from 'react';
 import ProjectCard from './ProjectCard';
 
 const Projects = () => {
+
+  const projects = [
+    {
+      title: "CFB Machine",
+      description: "A football website"
+    },
+    {
+      title: "Wacky Stories",
+      description: "MadLibz"
+    },
+    {
+      title: "Job Board",
+      description: "Hire Me"
+    },
+    {
+      title: "Study Buddy",
+      description: "I know it all"
+    }
+  ]
+
   return (
     <>
       <h1>Projects</h1>
       <div className="card-container">
-        <ProjectCard
-          title="CFB Machine"
-          description="A football website"
-        />
+        {projects.map(proj => {
+          return (
+            <ProjectCard
+              title={proj.title}
+              description={proj.description}
+            />
+          )
+        })}
+        
       </div>
     </>
   );
