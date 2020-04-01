1dfa1df7bc9306d27f394c70189f2e00ee017031
Andrew Davisson <akdavisson4@gmail.com>
2020-03-31 23:03:28 -0600

    added a lot more projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,27 +5,47 @@ const Projects = () => {
 
   const projects = [
     {
+      title: "Job Board",
+      description: "This application allows a user to view companies and job postings, and save contact information and notes. This project also implements GraphQL.",
+      repository: "https://github.com/adavisson/job-board-client"
+    },
+    {
       title: "CFB Machine",
       description: "This application allows allows you to look up historical College Football matches and stats.",
       repository: "https://github.com/adavisson/cfb-machine",
       link: "https://cfb-machine.andydavisson.com/"
     },
     {
+      title: "Ansible Home",
+      description: "A small Ansible project for managing my home media server and my AWS hosted webserver.",
+      repository: "https://github.com/adavisson/ansible-home"
+    },
+    {
       title: "Crazy Tales",
       description: "This application mimics Mad Libs. A user is presented with types of words to come up with and is then presented with a story where those words are used.",
-      repository: "https://github.com/adavisson/Wacky-Stories",
+      repository: "https://github.com/adavisson/crazy-tales",
       link: "https://crazy-tales.andydavisson.com/"
     },
     {
-      title: "Job Board",
-      description: "This application allows a user to view companies and job postings, and save contact information and notes. This project also implements GraphQL.",
-      repository: "https://github.com/adavisson/job-board-client"
+      title: "Meme Generator",
+      description: "This application has several popular images used in memes and allows you to add a phrase. It also allows you to choose the placement and color of the phrase.",
+      repository: "https://github.com/adavisson/meme-generator"
+    },
+    {
+      title: "Rails Library",
+      description: "This is a full Ruby on Rails application that mimics a library. It allows Librarians to add books, authors, and genres, and it allows standard users to check in/out books.",
+      repository: "https://github.com/adavisson/rails-project-library"
     },
     {
       title: "Study Buddy",
       description: "This application provides study questions for various topics that one might see in a developer interview.",
       repository: "https://github.com/adavisson/study-buddy",
       link: "https://study-buddy.andydavisson.com/"
+    },
+    {
+      title: "Beer Presenter",
+      description: "This is a command line Ruby application that scrapes the site Untappd for the top 50 beers, and allows you to view them by genre and brewery.",
+      repository: "https://github.com/adavisson/beer_presenter"
     }
   ]
 
