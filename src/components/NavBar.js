5adc72cae537bfb35bf8f22991dc9a4bbc45c201
Andrew Davisson <akdavisson4@gmail.com>
2020-04-13 12:22:03 -0600

    added hyperlink for weather page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ import { usePosition } from 'use-position';
 const NavBar = () => {
   const [iconCode, setIconCode] = useState("")
   const [city, setCity] = useState("")
+  const [cityId, setCityId] = useState('5419384')
   const [temp, setTemp] = useState("")
   const appId = '440287f1b78a560637a7abe6f38d3739'
   const {latitude, longitude, error} = usePosition();
@@ -13,9 +14,11 @@ const NavBar = () => {
     const setData = async () => {
       const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude || 39.74}&lon=${longitude || -104.99}&units=imperial&appid=${appId}`)
       const data = await result.json()
+      console.log(data)
       setCity(data.name)
       setTemp(data.main.temp)
       setIconCode(data.weather[0].icon)
+      setCityId(data.id)
     }
     setData()
   }, [latitude, longitude])
@@ -37,9 +40,10 @@ const NavBar = () => {
         </Nav>
       </Navbar.Collapse>
       <Nav className="weather">
-        <Navbar.Text>{`${city} ${temp}\xB0 `}
+        <Nav.Link href={`https://openweathermap.org/city/${cityId}`} target="_blank">
+          {`${city} ${temp}\xB0 `}
           <img alt="weather icon" src={`https://openweathermap.org/img/wn/${iconCode}.png`} />
-        </Navbar.Text>
+        </Nav.Link>
       </Nav>
     </Navbar>
   );
