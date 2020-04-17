2d2d41be57ffb893108e98a74b0ab3a065680d69
Andrew Davisson <akdavisson4@gmail.com>
2020-04-17 10:25:37 -0600

    removed comments

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Card, Button } from 'react-bootstrap';
 import { Card, CardContent, Button, Typography, CardActions } from '@material-ui/core'
 import { cardStyle } from '../styles/theme'
 
@@ -7,18 +6,6 @@ const ProjectCard = (props) => {
 
   const classes = cardStyle();
   return (
-    // <Card className="text-center">
-    //   <Card.Header>{props.title}</Card.Header>
-    //   <Card.Body>
-    //     <Card.Text><em>{props.description}</em></Card.Text>
-    //     <Button variant="primary" href={props.repository} target="_blank">GitHub</Button>
-    //   </Card.Body>
-    //   {props.link && (
-    //     <Card.Footer>
-    //       <Button style={{ width: "50%"}} variant="secondary" href={props.link} target="_blank">Demo</Button>
-    //     </Card.Footer>
-    //   )}
-    // </Card>
     <Card className={classes.root} color="primary">
       <CardContent>
         <Typography className={classes.title} color="secondary">
