9af94dac408db967b70e80b0709aa881d1cfa0e9
Andrew Davisson <akdavisson4@gmail.com>
2020-04-17 09:08:08 -0600

    testing material button

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,51 +1,57 @@
 //import React from 'react';
-import React, { Component } from 'react';
-import { Document, Page } from 'react-pdf';
-import { pdfjs } from 'react-pdf';
-import resume from "../files/andrew-resume.pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import React, { Component } from 'react'
+import { Document, Page } from 'react-pdf'
+import { pdfjs } from 'react-pdf'
+import resume from '../files/andrew-resume.pdf'
+import { Button } from '@material-ui/core'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 class Resume extends Component {
   state = {
-    w: window.innerWidth
+    w: window.innerWidth,
   }
 
-  componentDidMount(){
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
+  componentDidMount() {
+    this.updateWindowDimensions()
+    window.addEventListener('resize', this.updateWindowDimensions)
   }
 
-  componentWillUnmount(){
-    window.removeEventListener('resize', this.updateWindowDimensions);
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions)
   }
 
   updateWindowDimensions = () => {
-    if(window.innerWidth < 700){
+    if (window.innerWidth < 700) {
       this.setState({
-        w: window.innerWidth * .9
+        w: window.innerWidth * 0.9,
       })
     } else {
       this.setState({
-        w: null
+        w: null,
       })
     }
   }
 
-  render() { 
+  render() {
     return (
       <div className="resume">
-       <h1>Resume</h1>
-       <div className="link">
-         <a target="_blank" href={resume}>Download a copy</a>
-       </div>
-       <div className="document">
-         <Document file={resume}>
-           <Page pageNumber={1} width={this.state.w}/>
-         </Document>
-       </div>
-     </div>
-    );
+        <h1>Resume</h1>
+        <div className="link">
+          {/* <a target="_blank" href={resume}>
+            Download a copy
+          </a> */}
+          <Button variant="contained" color="primary" href={resume} target="_blank">
+            Download a Copy
+          </Button>
+        </div>
+        <div className="document">
+          <Document file={resume}>
+            <Page pageNumber={1} width={this.state.w} />
+          </Document>
+        </div>
+      </div>
+    )
   }
 }
- 
-export default Resume;
+
+export default Resume
