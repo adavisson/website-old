d6864117458586e0aad75e9f2b5e5ccee7f418c7
Andrew Davisson <akdavisson4@gmail.com>
2020-04-17 10:21:47 -0600

    added material theming to projects page

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+// import { Card, Button } from 'react-bootstrap';
+import { Card, CardContent, Button, Typography, CardActions } from '@material-ui/core'
+import { cardStyle } from '../styles/theme'
 
 const ProjectCard = (props) => {
+
+  const classes = cardStyle();
   return (
-    <Card className="text-center">
-      <Card.Header>{props.title}</Card.Header>
-      <Card.Body>
-        <Card.Text><em>{props.description}</em></Card.Text>
-        <Button variant="primary" href={props.repository} target="_blank">GitHub</Button>
-      </Card.Body>
-      {props.link && (
-        <Card.Footer>
-          <Button style={{ width: "50%"}} variant="secondary" href={props.link} target="_blank">Demo</Button>
-        </Card.Footer>
-      )}
+    // <Card className="text-center">
+    //   <Card.Header>{props.title}</Card.Header>
+    //   <Card.Body>
+    //     <Card.Text><em>{props.description}</em></Card.Text>
+    //     <Button variant="primary" href={props.repository} target="_blank">GitHub</Button>
+    //   </Card.Body>
+    //   {props.link && (
+    //     <Card.Footer>
+    //       <Button style={{ width: "50%"}} variant="secondary" href={props.link} target="_blank">Demo</Button>
+    //     </Card.Footer>
+    //   )}
+    // </Card>
+    <Card className={classes.root} color="primary">
+      <CardContent>
+        <Typography className={classes.title} color="secondary">
+          {props.title}
+        </Typography>
+        <Typography className={classes.body} color="primary">
+          {props.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+      <Button variant="contained" color="secondary">
+          Github
+        </Button>
+        {props.link && (
+          <Button variant="contained" color="secondary">
+            Demo
+          </Button>
+        )}
+      </CardActions>
     </Card>
   );
 }
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,4 +1,4 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import { createMuiTheme, makeStyles } from '@material-ui/core/styles'
 
 export const theme = createMuiTheme({
   palette: {
@@ -9,12 +9,30 @@ export const theme = createMuiTheme({
     secondary: {
       main: '#ffb347',
       contrastText: '#003866',
-    }
+    },
   },
   typography: {
-    fontFamily: [
-      '"Titillium Web"',
-      'sans-serif'
-    ].join(',')
-  }
+    fontFamily: ['"Titillium Web"', 'sans-serif'].join(','),
+  },
+})
+
+export const cardStyle = makeStyles({
+  root: {
+    width: '20rem',
+    margin: '15px',
+    padding: '5px',
+    backgroundColor: '#003866',
+  },
+  title: {
+    fontSize: 'x-large',
+    // margin: '5px'
+  },
+  body: {
+    margin: '5px',
+    padding: '15px',
+    backgroundColor: '#ffb347'
+  },
+  pos: {
+    marginBottom: 12,
+  },
 })
